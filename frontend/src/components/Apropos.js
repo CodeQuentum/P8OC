b9c47cd51c18ad1d5fd47d55e9bae7c5772a1915
Quentin Nicot <q.nicot@icloud.com>
2023-10-02 20:03:02 +0200

    ajout d'une image dans le background de intro et correction d'orthographe

--- frontend/src/components/Apropos.js
+++ frontend/src/components/Apropos.js
@@ -7,11 +7,11 @@ function Apropos(){
         <div className='apropos-container'>
         <div className='introComponent'>
             <h2>A propos</h2>
-        <p>Bienvenue sur mon portfolio, je m'appelle Quentin, et je suis un jeune developper web
-            suite à une formation chez Openclassroom, dans la quelle j'ai pu apprendre les bases
-            du metier. J'ai encore beaucoup de choses à apprendre mais grace aux differents projets 
-            réalisés durant ma formation je suis desormais capable de mettre mes nouvelles competences
-            aux services de vos projets.
+        <p>Bienvenue sur mon portfolio, je m'appelle Quentin. Je suis un jeune developper web.
+            A la suite d'une formation chez Openclassroom, dans laquelle j'ai pu apprendre les bases
+            du métier, grâce aux différents projets réalisés durant ma formation, je suis désormais capable 
+            de mettre mes nouvelles compétence aux services de vos projets. Je suis très optimiste à l'idée 
+            de tout ce qu'il me reste à découvrir en pratiquant ce nouveau métier.
         </p>
         </div>
         </div>
--- frontend/src/components/Intro.js
+++ frontend/src/components/Intro.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import '../styles/Intro.css';
+import backgroundImg from '../assets/background-intro.jpeg'; 
+
 
 function Intro() {
+  const introStyle = {
+    backgroundImage: `url(${backgroundImg})`,
+  };
+
   return (
-    <div className="intro-container">
+    <div className="intro-container" style={introStyle}>
       <h1 className="typing-effect">Quentin, Développeur Web.</h1>
       <p>Bienvenue sur mon portfolio</p>
       <hr className="intro-divider" />
