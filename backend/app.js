e49406ac944a5c460a2f0e70b9b1f2a4ffa071c0
Quentin Nicot <q.nicot@icloud.com>
2023-11-01 17:43:27 +0100

    connection a mongodb securisé avec dotenv

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,46 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('Requête recue !');
-  next();
-});
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+    .then(() => {
+      console.log('Connecté à MongoDB');
+    })
+    .catch((error) => {
+      console.error('Erreur de connexion à MongoDB :', error);
+    });
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
+app.use('/api/projects', (req, res, next) => {
+    const stuff = [
+      {
+        _id: 'oeihfzeoi',
+        title: 'Mon premier objet',
+        description: 'Les infos de mon premier objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 4900,
+        userId: 'qsomihvqios',
+      },
+      {
+        _id: 'oeihfzeomoihi',
+        title: 'Mon deuxième objet',
+        description: 'Les infos de mon deuxième objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 2900,
+        userId: 'qsomihvqios',
+      },
+    ];
+    res.status(200).json(stuff);
+  });
 
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
-});
-
+  app.post('/api/projects', (req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({
+      message: 'Objet créé !'
+    });
+  });
 module.exports = app;
